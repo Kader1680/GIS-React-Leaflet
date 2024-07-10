fe4297cfa1074f420f6080a6d31849941d8b2b6c
Kader1680 <ouldhenniabaghdad@gmail.com>
2024-07-10 16:02:51 +0100

    upload project

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,25 @@
 
-const express = require("express")
-const app = express()
-app.listen(3000, ()=>{
-    console.log("server start at 3000")
+http = require('node:http');
+var express = require('express');
+var app = express();
+
+app.get('/', function (req, res) {
+    res.send("dfdfdfdf");
 })
+server = http.createServer(app);
+server.listen(5001);
+
+const mongoose = require("mongoose")
+const url = "mongodb+srv://user:0000@cluster0.rbzjgeh.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+mongoose.connect(url).then(()=>{
+    console.log("mongodb well conected")
+})
+.catch((err)=>{
+    console.log(err)
+})
+
+
+
+
+
+console.log('Server running at http://127.0.0.1:5001/');
